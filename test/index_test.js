65097b8f3328457ced2f38cb7bb2de9a38647320
Marcus Cavanaugh <m@mcav.com>
2013-09-27 10:54:23 -0700

    Add pop3 support

--- test/index_test.js
+++ test/index_test.js
@@ -2,6 +2,7 @@ suite('fakemail', function() {
   var fakemail = require('..'),
       net = require('net'),
       IMAPStack = require('../lib/imap_stack'),
+      POP3Stack = require('../lib/pop3_stack'),
       ControlServer = require('../lib/control_server');
 
   var creds = { user: 'testy', password: 'testy' };
@@ -21,9 +22,18 @@ suite('fakemail', function() {
     subject && subject.kill();
   });
 
-  function createStack() {
+  function createImapStack() {
     setup(function(done) {
       subject.createImapStack({ credentials: creds }, function(err, _stack) {
+        stack = _stack;
+        done(err);
+      });
+    });
+  }
+
+  function createPop3Stack() {
+    setup(function(done) {
+      subject.createPop3Stack({ credentials: creds }, function(err, _stack) {
         stack = _stack;
         done(err);
       });
@@ -53,7 +63,7 @@ suite('fakemail', function() {
   });
 
   test('#createImapStack', function() {
-    createStack();
+    createImapStack();
 
     test('stack details', function() {
       assert.ok(stack instanceof IMAPStack);
@@ -64,9 +74,9 @@ suite('fakemail', function() {
 
     test('smtp server', function(done) {
       read(stack.smtpPort, function(err, line, socket) {
-        if (err) return callback(err);
+        if (err) return done(err);
         // 220 is SMTP ready
-        assert.ok(line.match(/220/i), line + 'does not contain "imap"');
+        assert.ok(line.match(/220/i), line + 'does not contain "220"');
         socket.end();
         done();
       });
@@ -74,7 +84,7 @@ suite('fakemail', function() {
 
     test('imap server', function(done) {
       read(stack.imapPort, function(err, line, socket) {
-        if (err) return callback(err);
+        if (err) return done(err);
         assert.ok(line.match(/imap/i), 'line connect contains "imap"');
         socket.end();
         done();
@@ -82,8 +92,38 @@ suite('fakemail', function() {
     });
   });
 
-  suite('#cleanupStacks', function() {
-    createStack();
+  test('#createPop3Stack', function() {
+    createPop3Stack();
+
+    test('stack details', function() {
+      assert.ok(stack instanceof POP3Stack);
+      assert.ok(stack.pop3Port, 'has pop3 port');
+      assert.ok(stack.smtpPort, 'has smtp port');
+      assert.ok(stack.controlUrl, 'has control url');
+    });
+
+    test('smtp server', function(done) {
+      read(stack.smtpPort, function(err, line, socket) {
+        if (err) return done(err);
+        // 220 is SMTP ready
+        assert.ok(line.match(/220/i), line + 'does not contain "220"');
+        socket.end();
+        done();
+      });
+    });
+
+    test('pop3 server', function(done) {
+      read(stack.pop3Port, function(err, line, socket) {
+        if (err) return done(err);
+        assert.ok(line.match(/pope/i), 'line connect contains "pop3"');
+        socket.end();
+        done();
+      });
+    });
+  });
+
+  suite('#cleanupImapStacks', function() {
+    createImapStack();
 
     function isClosed(port, callback) {
       net.connect(port).on('error', function(err) {
@@ -98,6 +138,29 @@ suite('fakemail', function() {
 
     test('closes imap', function(done) {
       isClosed(stack.imapPort, done);
+    });
+
+    test('closes smtp', function(done) {
+      isClosed(stack.smtpPort, done);
+    });
+  });
+
+  suite('#cleanupPop3Stacks', function() {
+    createPop3Stack();
+
+    function isClosed(port, callback) {
+      net.connect(port).on('error', function(err) {
+        assert.equal(err.code, 'ECONNREFUSED');
+        callback();
+      });
+    }
+
+    setup(function(done) {
+      subject.cleanupStacks(done);
+    });
+
+    test('closes pop3', function(done) {
+      isClosed(stack.pop3Port, done);
     });
 
     test('closes smtp', function(done) {
